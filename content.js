5a929bdc012c2e8205db2f1a62115afa9155939c
Rohit Kumawat <rkumawat125@gmail.com>
2022-06-19 17:12:56 +0530

    v1.1 updates

--- content.js
+++ content.js
@@ -1,25 +1,72 @@
-function getClickMoreButton() {
-  const list = document.getElementsByTagName("div");
-  for (let i = 0; i < list.length; i++) {
-    if (list[i].textContent === "Show More Orders") {
-      return list[i];
+function timer(num) {
+  const el = document.getElementsByClassName("_2pWZz")[0];
+  setTimeout(() => {
+    if(el.textContent === "Loading...") {
+      timer(num);
+    } else {
+      clickMore(num);
     }
-  }
+  }, 100);
 }
-function clickMore(prevNum, el) {
+
+function getYear(str) {
+  return str.split("|")[1].trim().split(",")[2].trim();
+}
+
+function clickMore(prevNum) {
+  const el = document.getElementsByClassName("_2pWZz")[0];
   const currentNum = document.getElementsByTagName("div").length;
   if (currentNum !== prevNum && el.textContent === "Show More Orders") {
     el.click();
-    setTimeout(() => {
-      clickMore(currentNum, el);
-    }, 600);
+    timer(currentNum);
   } else {
     let totalMoney = 0;
     const list = document.getElementsByClassName("_3Hghg");
+    const yearEl = document.getElementsByClassName("_2uT6l");
+    const dishEl = document.getElementsByClassName("nRCg_");
+    let max = 0;
+    const yearObj = {};
+    const favDishObj = {};
+    let topFiveArr = [];
+
     for (let i = 0; i < list.length; i++) {
+      const year = getYear(yearEl[i].textContent);
+      const dish = dishEl[i].textContent.split(',');
+
+      if(yearObj[year]) {
+        yearObj[year] += Number(list[i].innerText)
+      } else {
+        yearObj[year] = Number(list[i].innerText)
+      }
+
+      for(let j=0; j<dish.length; j++) {
+        const dishName = dish[j].trim().split('x').map(el => el.trim());
+        const key = dishName[0].split("(")[0].trim();
+        if(favDishObj[key]) {
+          favDishObj[key] += Number(dishName[1]);
+        } else {
+          favDishObj[key] = Number(dishName[1]);
+        }
+
+        if(favDishObj[key] > max) {
+          max = favDishObj[key];
+        }
+      }
+
       totalMoney += Number(list[i].innerText);
     }
-    chrome.runtime.sendMessage({ totalMoney: totalMoney });
+
+    const dishesArr = [];
+    for(let key in favDishObj) {
+      dishesArr.push({[key]: favDishObj[key]});
+    }
+    dishesArr.sort((a,b) => {
+      return Object.values(b)[0] - Object.values(a)[0]
+    }).slice(0,5).map(el => topFiveArr.push(Object.keys(el)[0]));
+
+    yearObj['Total'] = totalMoney;
+    chrome.storage.local.set({totalMoney, yearObj, topFiveArr});
+    chrome.runtime.sendMessage({totalMoney, yearObj, topFiveArr});
     return;
   }
 }
@@ -41,8 +88,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         sendResponse({ message: "found_account" });
         list[i].click();
         setTimeout(() => {
-          const el = getClickMoreButton();
-          clickMore(1, el);
+          clickMore(1);
         }, 1000);
       }
     }
--- popup.js
+++ popup.js
@@ -1,3 +1,15 @@
+// storage checking
+chrome.storage.local.get("totalMoney", function (result) {
+  document.getElementById("lastChecked").innerText = `₹ ${result?.totalMoney || 0}`;
+})
+chrome.storage.local.get("topFiveArr", function (result) {
+  if (Array.isArray(result?.topFiveArr)) {
+    createFavItems(result.topFiveArr);
+    document.getElementById("notFoundInfo").style.display = "none";
+  }
+})
+
+// chrome apis
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var activeTab = tabs[0];
   if (activeTab && activeTab.url.includes("https://www.swiggy.com")) {
@@ -7,31 +19,81 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       function (response) {
         if (response?.message === "not_logged_in") {
           document.getElementById("link").disabled = true;
+        } else if (response?.message === "logged_in") {
+          document.getElementById("instructions").style.display = "none";
         }
       }
     );
   } else {
     document.getElementById("link").disabled = true;
   }
+  document.getElementById("dropHead").style.opacity = 0.5;
+  document.getElementById("dropHead").style.cursor = "not-allowed";
 });
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.totalMoney >= 0) {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("show-total").innerText = `₹ ${request.totalMoney}`;
+    document.getElementById("years").innerText = "";
+    appendDropdownOption(request.yearObj);
+    document.getElementsByClassName("dish-list")[0].innerText = "";
+    createFavItems(request.topFiveArr);
+    document.getElementById("link").disabled = false;
+    document.getElementById("dropHead").style.opacity = 1;
+    document.getElementById("dropHead").style.cursor = "pointer";
+  }
+});
+
+// Events
 document.getElementById("link").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(
       tabs[0].id,
       { message: "search_account" },
       function (response) {
-        console.log({ response });
-        if (response.message === "found_account") {
-          document.getElementById("show-total").innerText = "Loading...";
+        if (response?.message === "found_account") {
+          document.getElementById("link").disabled = true;
+          document.getElementById("loader").style.display = "block";
+          document.getElementById("show-total").innerText = "";
         }
       }
     );
   });
 });
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.totalMoney >= 0) {
-    document.getElementById("show-total").innerText = request.totalMoney;
+document.getElementById("years").addEventListener("click", (event) => {
+  const val = event.target.value;
+  chrome.storage.local.get("yearObj", function (result) {
+    document.getElementById("show-total").innerText = `₹ ${result.yearObj[val]}`;
+    document.getElementById("years").style.display = "none";
+    document.getElementById("dropHead").innerHTML = `${val} &#9660;`;
+  });
+})
+document.getElementById("dropHead").addEventListener("click", () => {
+  if (document.getElementById("years").style.display === "none" || document.getElementById("years").style.display === "") {
+    document.getElementById("years").style.display = "block";
+  } else {
+    document.getElementById("years").style.display = "none";
   }
-});
+})
+
+// common functions
+function appendDropdownOption(obj) {
+  Object.keys(obj).reverse().forEach((key) => {
+    const div = document.createElement("div");
+    div.value = key;
+    div.innerText = key;
+    document.getElementById("years").appendChild(div);
+  })
+}
+function createFavItems(arr) {
+  arr.forEach((dish, index) => {
+    const divEl = document.createElement("div");
+    if (index !== 0) {
+      divEl.innerText = `${index + 1}: ${dish}`;
+    } else {
+      divEl.innerHTML = `${index + 1}: ${dish} &#127942`;
+    }
+    document.getElementsByClassName("dish-list")[0].appendChild(divEl);
+  })
+}
